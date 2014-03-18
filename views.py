c627e69290ceab7a0edefbf2b9652b633851d468
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-03-18 06:13:30 +0000

    Dashboard changes

--- views.py
+++ views.py
@@ -5,5 +5,6 @@ from archiver.models import ArchiverApp
 # Create your views here.
 @dashboard_view
 def dashboard(request, context):
+    context['creators'] = ArchiverApp.objects.get(app_label='acm_members').creators.all()
     context['members'] = ArchiverApp.objects.get(app_label='acm_members').members.all()
     return render_to_response('acm_members/dashboard.html', context)
